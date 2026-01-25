43ee31f3924caaada947ae808b679897d0abccb7
shashank-aidev <shashank.chauhan.ai@gmail.com>
2026-01-25 22:05:13 +0530

    Delete string.py

--- string.py
+++
@@ -1,4 +0,0 @@
-name ="shashank"
-print(name.capitalize())#It capitals the ffirs tletter of the string
-print(name.endswith("nk"))#it will check if the name is ending with "nk" or not then give output in boolean
-print(name.startswith("s"))#same as above

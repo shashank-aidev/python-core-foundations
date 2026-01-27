6aee85bb579e61632ff362698959a63beb5b174f
Shashank <shashank.chauhan.ai@gmail.com>
2026-01-27 07:46:59 +0530

    09_file_handling_with.py

--- 09_file_handling_with.py
+++ 09_file_handling_with.py
@@ -36,6 +36,49 @@ but using "with" to assecc file we don't have to close the file it will close au
 
 '''
 
-with open("file.txt","r") as f:
+with open("file.txt","r") as f: # you can create any file or access it by the given path
     for line in f:
         print(line)
+
+
+
+#Solving 3 problems to understed it more 
+
+
+#problem 1  image an data.txt is file and content is 
+#A
+#B  is written on it
+# with open("data.txt", "w") as f:
+#     f.write("C\n") # this will clear the whole file and rewrite the file again
+#     # It will write again the file with "C"
+
+# with open("data.txt", "a") as f:
+#     f.write("D\n") #now it will update the existing file So it will and "D" after "C".
+#output will be C\n D
+
+
+#problem 2
+# with open("data.txt", "r") as f:
+#     print(f.readline())#It will print the first line of the file 
+#     for line in f:
+#         print(line)#It will peint the whole content of the file, except the first line which is allready readed
+#output will be 
+'''
+one
+two
+three'''
+
+#problem 3
+with open("data.txt", "r") as f:
+    content1 = f.read()#it read the whole file 
+    # content2 = f.read()# it also does the same 
+
+print(content1)
+# print(content2)
+#output will be :
+'''
+hello 
+world
+hello 
+world
+'''

6c816107f6ddc104727617f37c71c2bbcb1996de
Shashank <shashank.chauhan.ai@gmail.com>
2026-01-27 07:48:07 +0530

    09_file_handling_with.py

--- 09_file_handling_with.py
+++ 09_file_handling_with.py
@@ -48,20 +48,20 @@ with open("file.txt","r") as f: # you can create any file or access it by the gi
 #problem 1  image an data.txt is file and content is 
 #A
 #B  is written on it
-# with open("data.txt", "w") as f:
-#     f.write("C\n") # this will clear the whole file and rewrite the file again
-#     # It will write again the file with "C"
+with open("data.txt", "w") as f:
+    f.write("C\n") # this will clear the whole file and rewrite the file again
+    # It will write again the file with "C"
 
-# with open("data.txt", "a") as f:
-#     f.write("D\n") #now it will update the existing file So it will and "D" after "C".
+with open("data.txt", "a") as f:
+    f.write("D\n") #now it will update the existing file So it will and "D" after "C".
 #output will be C\n D
 
 
 #problem 2
-# with open("data.txt", "r") as f:
-#     print(f.readline())#It will print the first line of the file 
-#     for line in f:
-#         print(line)#It will peint the whole content of the file, except the first line which is allready readed
+with open("data.txt", "r") as f:
+    print(f.readline())#It will print the first line of the file 
+    for line in f:
+        print(line)#It will peint the whole content of the file, except the first line which is allready readed
 #output will be 
 '''
 one
@@ -71,14 +71,28 @@ three'''
 #problem 3
 with open("data.txt", "r") as f:
     content1 = f.read()#it read the whole file 
-    # content2 = f.read()# it also does the same 
+    content2 = f.read()# it also does the same 
 
 print(content1)
-# print(content2)
+print(content2)
 #output will be :
 '''
 hello 
 world
-hello 
-world
+
+As i was thinking it will not print the same content of the file again and agian i wonher why?
+'''
+
+
+'''
+#Important
+First read() → reads whole file
+
+#file
+Hello 
+world | <- This is pointer
+
+Pointer now at EOF
+Second read() → nothing left → returns ""
+
 '''
